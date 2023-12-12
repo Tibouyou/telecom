472dce856aa604511a75e6351c874c5382b1e1a8
Tibouyou <tibouyou01@gmail.com>
2023-12-12 09:21:12 +0100

    zoom now change layer on map

--- app/components/Map.js
+++ app/components/Map.js
@@ -1,12 +1,17 @@
 // components/Map.js
 "use client"
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import { MapContainer, TileLayer, GeoJSON, LayersControl } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import styles from '../styles/map.css'
-//import departements from '../data/departements.json'
-//import communes from '../data/communes.json'
-import RecenterButton from './RecenterButton'
+import departements from '../data/departements.json'
+import communes from '../data/communes.json'
+import { useMapEvents } from 'react-leaflet'
+import { useLeafletContext } from '@react-leaflet/core'
+import L from 'leaflet'
+
+const communesLayer = L.geoJSON(communes, { onEachFeature: randomColor });
+const departementsLayer = L.geoJSON(departements, { onEachFeature: randomColor });
 
 function randomColor(feature, layer) {
   const popupContent = `
@@ -14,7 +19,7 @@ function randomColor(feature, layer) {
       <p>${feature.properties.dep}</p>       
     </div>
   `;
-  layer.bindPopup(popupContent);
+  //layer.bindPopup(popupContent);
   var colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'black'];
   var color = colors[Math.floor(Math.random() * colors.length)];
   layer.setStyle({
@@ -23,6 +28,34 @@ function randomColor(feature, layer) {
     opacity: 0.5,
     fillOpacity: 0.2
   });
+}
+
+function HandleZoom() {
+  const context = useLeafletContext();
+  const container = context.layerContainer || context.map;
+  
+  const mapEvents = useMapEvents({
+      zoomend: () => {
+        const zoomLevel = mapEvents.getZoom();
+          if (zoomLevel >= 9) {
+            if (container.hasLayer(communesLayer)) {
+              return null;
+            } else {
+              container.addLayer(communesLayer);
+            container.removeLayer(departementsLayer);
+            }
+          }
+          if (zoomLevel < 9) {
+            if (container.hasLayer(departementsLayer)) {
+              return null;
+            } else {
+              container.addLayer(departementsLayer);
+              container.removeLayer(communesLayer);
+            }
+          }
+      },
+  });
+  return null;
 }
 
 export default function Map( { center, zoom, innerRef } ) {
@@ -38,7 +71,7 @@ export default function Map( { center, zoom, innerRef } ) {
             <div className='MapContainer-map'>
               <MapContainer className='map' center={center} zoom={zoom} ref={innerRef}>
                 <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
-                {/* <GeoJSON data={communes} onEachFeature={randomColor} /> */}
+                <HandleZoom />
               </MapContainer>
             </div>
           </div>
