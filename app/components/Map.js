ef7e014d445c6415b9efd2d31c52a23766933eae
Tibouyou <tibouyou01@gmail.com>
2024-01-05 00:16:46 +0100

    select data to display

--- app/components/Map.js
+++ app/components/Map.js
@@ -1,6 +1,6 @@
 // components/Map.js
 "use client"
-import React, {useRef, useState} from 'react'
+import React, { useRef, useState } from 'react'
 import { MapContainer, TileLayer, GeoJSON, LayersControl } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import styles from '../styles/map.css'
@@ -11,87 +11,113 @@ import { useLeafletContext } from '@react-leaflet/core'
 import L from 'leaflet'
 
 import communesData from '../data/communesData.json'
+import depData from '../data/depData.json'
 
-const communesLayer = L.geoJSON(communes, { onEachFeature: randomColorCom });
-const departementsLayer = L.geoJSON(departements, { onEachFeature: randomColor });
+var currentFilter = 'elig_thd1g';
+const communesLayer = L.geoJSON(communes, { style: styleCom, onEachFeature: popupCom });
+const departementsLayer = L.geoJSON(departements, { style: styleDep, onEachFeature: popupDep });
 
-function randomColorCom(feature, layer) {
-
-  var eligibility = communesData['array'][feature.properties.codgeo]['elig_thd1g']/communesData['array'][feature.properties.codgeo]['nbr']
-  var colors = ['#f1eef6','#bdc9e1','#74a9cf','#2b8cbe','#045a8d'];
-  var color = colors[Math.floor(eligibility * (colors.length))];
+function styleCom(feature) {
+  var color;
+  var colors = ['#f1eef6', '#bdc9e1', '#74a9cf', '#2b8cbe', '#045a8d'];
+  let eligibility = communesData['array'][feature.properties.codgeo][currentFilter] / communesData['array'][feature.properties.codgeo]['nbr']
   if (eligibility == 1) {
     color = colors[4];
-  }
+  } else color = colors[Math.floor(eligibility * (colors.length))];
+  console.log(eligibility);
+  return {
+    weight: 1,
+    opacity: 1,
+    color: color,
+    fillOpacity: 0.7
+  };
+}
+
+function styleDep(feature) {
+  var color;
+  var colors = ['#f1eef6', '#bdc9e1', '#74a9cf', '#2b8cbe', '#045a8d'];
+  let eligibility = depData['array'][feature.properties.code_insee][currentFilter] / depData['array'][feature.properties.code_insee]['nbr']
+  if (eligibility == 1) {
+    color = colors[4];
+  } else color = colors[Math.floor(eligibility * (colors.length))];
+  return {
+    weight: 1,
+    opacity: 1,
+    color: color,
+    fillOpacity: 0.7
+  };
+}
+
+function popupDep(feature, layer) {
+  var eligibility = depData['array'][feature.properties.code_insee]['elig_thd1g'] / depData['array'][feature.properties.code_insee]['nbr']
   const popupContent = `
     <div>
-      <p>${communesData['array'][feature.properties.codgeo]['nom_com']}</p>  
-      <p>Eligibilité THD : ${Math.round(eligibility*100)}%</p>
-      <p>Color : ${color}</p>     
+      <p>${feature.properties.nom}</p>
+      <p>Eligibilité THD : ${Math.round(eligibility * 100)}%</p> 
     </div>
   `;
   layer.bindPopup(popupContent);
-  layer.setStyle({
-    color: color,
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.7
-  });
 }
 
-function randomColor(feature, layer) {
+function popupCom(feature, layer) {
+  var eligibility = communesData['array'][feature.properties.codgeo]['elig_thd1g'] / communesData['array'][feature.properties.codgeo]['nbr']
   const popupContent = `
     <div>
-      <p>${feature.properties.dep}</p>       
+      <p>${communesData['array'][feature.properties.codgeo]['nom_com']}</p>  
+      <p>Eligibilité THD : ${Math.round(eligibility * 100)}%</p>    
     </div>
   `;
-  //layer.bindPopup(popupContent);
-  var colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'black'];
-  var color = colors[Math.floor(Math.random() * colors.length)];
-  layer.setStyle({
-    color: color,
-    weight: 0.5,
-    opacity: 0.5,
-    fillOpacity: 0.2
-  });
+  layer.bindPopup(popupContent);
 }
 
 function HandleZoom() {
   const context = useLeafletContext();
   const container = context.layerContainer || context.map;
-  
+
   const mapEvents = useMapEvents({
-      zoomend: () => {
-        const zoomLevel = mapEvents.getZoom();
-          if (zoomLevel >= 9) {
-            if (container.hasLayer(communesLayer)) {
-              return null;
-            } else {
-              container.addLayer(communesLayer);
-            container.removeLayer(departementsLayer);
-            }
-          }
-          if (zoomLevel < 9) {
-            if (container.hasLayer(departementsLayer)) {
-              return null;
-            } else {
-              container.addLayer(departementsLayer);
-              container.removeLayer(communesLayer);
-            }
-          }
-      },
+    zoomend: () => {
+      const zoomLevel = mapEvents.getZoom();
+      if (zoomLevel >= 9) {
+        if (container.hasLayer(communesLayer)) {
+          return null;
+        } else {
+          container.addLayer(communesLayer);
+          container.removeLayer(departementsLayer);
+        }
+      }
+      if (zoomLevel < 9) {
+        if (container.hasLayer(departementsLayer)) {
+          return null;
+        } else {
+          container.addLayer(departementsLayer);
+          container.removeLayer(communesLayer);
+        }
+      }
+    },
   });
   return null;
 }
 
-export default function Map( { center, zoom, innerRef } ) {
-
+export default function Map({ center, zoom, innerRef }) {
+  function changeFilter(value) {
+    currentFilter = value;
+    communesLayer.setStyle(styleCom);
+    departementsLayer.setStyle(styleDep);
+  }
   return (
     <>
       <div className='MapContainer-all'>
         <div className='MapContainer-titre'>
           <h1>Carte de couverture réseau française</h1>
         </div>
+        <select name="select" onChange={e => changeFilter(e.target.value)}>
+          <option value="elig_thd1g">1G/s</option>
+          <option value="elig_thd100">100M/s</option>
+          <option value="elig_thd30">30M/s</option>
+          <option value="elig_bhd8">8M/s</option>
+          <option value="elig_hd3">3M/s</option>
+          <option value="elig_hd05">0.5M/s</option>
+        </select>
         <div className='row'>
           <div className='col'>
             <div className='MapContainer-map'>
--- pages/api/getdata.js
+++ pages/api/getdata.js
@@ -11,14 +11,17 @@ export default async function handler(req, res) {
     });
     try {
 
-        const query = `SELECT * FROM communes`;
+        const query = `SELECT code_dep, SUM(elig_thd1g) as 'elig_thd1g', SUM(elig_thd100) as 'elig_thd100', SUM(elig_thd30) as 'elig_thd30', SUM(elig_bhd8) as 'elig_bhd8', 
+        SUM(elig_hd3) as 'elig_hd3', SUM(elig_hd05) as 'elig_hd05', SUM(inel_hd) as 'inel_hd', SUM(nbr) as 'nbr' FROM communes GROUP BY code_dep`;
         const values = []
         const [data] = await dbconnection.execute(query, values);
         dbconnection.end();
+        
         let array = {};
         for (var i = 0; i < data.length; i++) {
-            array[data[i].code_insee] = data[i];
+            array[data[i].code_dep] = data[i];
         }
+        
         res.status(200).json({ array });
     } catch (error) {
         res.status(500).json({ error: error.message });
